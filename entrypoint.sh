307a71e6b7e463eed8aeaf66a9bf1bfee695c7c6
Alex Seriy <alex.m.seriy@gmail.com>
2026-06-16 13:28:13 +0000

    Add CRDB to the ENTRYPOINT

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,22 @@ set -e
 # Setup a trap to kill all background processes if the script receives a SIGTERM/SIGINT
 trap 'kill $(jobs -p) 2>/dev/null' EXIT
 
-# Start process 1 in the background
-./process_one &
+JOIN="tasks.${SELF}:26258"
+for n in ${NEIGHBORS}; do
+    JOIN="${JOIN},tasks.${n}:26258"
+done
 
-# Start process 2 in the background
-./process_two &
+# Start process 1 in the background
+/cockroach/cockroach start \
+    --certs-dir=/cockroach/certs \
+    --listen-addr=:26258 --sql-addr=:26257 --http-addr=:8080 \
+    --advertise-addr=tasks.${SELF}:26258 \
+    --advertise-sql-addr=tasks.${SELF}:26257 \
+    --join=${JOIN} \
+    --store=/data &
+
+# # Start process 2 in the background
+# dbworkload run -w DatapointLogger.py --uri  &
 
 # Wait for ANY background process to exit or fail
 wait -n
